7eaef9a8ddd5244578ec3106d091a2fdb9c6a5e8
Lidvarim <76064778+naervyx@users.noreply.github.com>
2025-12-09 10:38:45 -0300

    Fix extension development path for tests

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -3,7 +3,7 @@ import { runTests } from '@vscode/test-electron';
 
 async function main() {
     try {
-        const extensionDevelopmentPath = path.resolve(__dirname, '../../');
+        const extensionDevelopmentPath = path.resolve(__dirname, '../../../');
         const extensionTestsPath = path.resolve(__dirname, './suite/index');
 
         await runTests({ extensionDevelopmentPath, extensionTestsPath });
